25d954a52f97185bf6563698595269b67f2fc848
Anmol Sidhu <anmol.sidhu@razorpay.com>
2024-03-11 22:27:59 +0530

    refactor

--- pongo.sh
+++ pongo.sh
@@ -118,7 +118,7 @@ function globals {
   KONG_OSS_TAG_POSTFIX="-ubuntu"
 
   # unoffical Kong CE images repo, the fallback
-  KONG_OSS_UNOFFICIAL_TAG_PREFIX="kong/kong:"
+  KONG_OSS_UNOFFICIAL_TAG_PREFIX="kong:"
   KONG_OSS_UNOFFICIAL_TAG_POSTFIX="-ubuntu"
 
   # development EE images repo, these are public, no credentials needed
